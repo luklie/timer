d2b7efb8d7d12c54f3e65cdc63e9caf7f1e58439
Lukasz Lieske <47499487+luklie@users.noreply.github.com>
2020-04-01 11:23:32 +0100

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,22 @@
+function counting()
+	{
+		var today = new Date();
+
+		var day = today.getDate();
+		var month = today.getMonth()+1;
+		var year = today.getFullYear();
+
+		var hour = today.getHours();
+		if (hour<10) hour = "0"+hour;
+
+		var minute = today.getMinutes();
+		if (minute<10) minute = "0"+minute;
+
+		var seconds = today.getSeconds();
+		if (seconds<10) seconds = "0"+seconds;
+
+		document.getElementById("timer").innerHTML = 
+			day+"/"+month+"/"+year+" | "+hour+":"+minute+":"+seconds;
+
+		setTimeout("counting()",1000);
+	}
